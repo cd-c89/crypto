6c99070c001afdda332e47c2a717ff527a79ba18
c <ckdeutschbein@willamette.edu>
2025-01-24 11:15:18 -0800

    tester.c

--- macros/tester.c
+++ macros/tester.c
@@ -1,37 +1,46 @@
-unsigned choice(unsigned a, unsigned b, unsigned c) {
-    asm (
-        "andl %0, %1;"
-        "notl %0;"
-        "andl %2, %0;"
-        "orl  %1, %0;"
-        : "+r" (a), "+r" (b)
-        : "r" (c)
-    );
-    return a;
-}
+#define S (sizeof(unsigned) << 3)
 
-unsigned median(unsigned a, unsigned b, unsigned c)  {
-    unsigned d;
-    asm (
-        "movl %0, %3;"
-        "andl %2, %3;"
-        "andl %1, %2;"
-        "andl %0, %1;" 
-        "orl  %3, %2;"
-        "orl  %2, %1;"
-        : "+r" (a), "+r" (b), "+r" (c), "+r" (d)
-    );
+char *u2b(unsigned u, char *b) {
+    unsigned i = 0;
+    for (; i < S; i++) { b[i] = (u >> i) & 1; }
     return b;
 }
 
+unsigned b2u(char *b) {
+    unsigned u = 0, i = 0;
+    for (; i < S; i++) { u += b[i] << i; }
+    return u;
+}
+
+unsigned choice(unsigned a, unsigned b, unsigned c) {
+    unsigned vals[4] = {a, b, c}, i = 0;
+    char buf[3][S];
+    for (i = 0; i < 3; i++) { u2b(vals[i], buf[i]); }
+    for (i = 0; i < S; i++) { buf[0][i] = buf[0][i] ? buf[1][i] : buf[2][i]; }
+    return b2u(buf[0]);
+}
+
+unsigned median(unsigned a, unsigned b, unsigned c)  {
+    unsigned vals[4] = {a, b, c}, i = 0;
+    char buf[3][S];
+    for (i = 0; i < 3; i++) { u2b(vals[i], buf[i]); }
+    for (i = 0; i < S; i++) { buf[0][i] = (buf[0][i] + buf[1][i] + buf[2][i]) >> 1; }
+    return b2u(buf[0]);
+}
+
 unsigned rotate(unsigned a, unsigned b) {
-    asm("rorl %%cl, %0" : "+r" (a) : "c" (b));
-    return a;
+    char buf[2][S];
+    unsigned i;
+    u2b(a, buf[0]);
+    for (i = 0; i < S; i++) { buf[1][i] = buf[0][(i + b) % S]; }
+    printf("%02x %b\n=? %b\n ==%b\n\n", b, a, b2u(buf[1]), ROTATE(a,b));
+    return b2u(buf[1]);
 }
 
 int main() {
     /* Various Variables*/
-    unsigned a[3], i, buf[8] = {0, 1, 2, 4, 8, 16, 24, 32};
+    unsigned a[3], i, buf[8] = 
+    {0, 1, 2, 4, 8, 16, 24, 32};
 
     /* Random values from memory address... */
     /* use "-Wno-pointer-to-int-cast" */
