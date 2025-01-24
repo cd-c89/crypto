9caedc61b0aacb10d8a53f4f41f1f53539a5f905
c <ckdeutschbein@willamette.edu>
2025-01-24 15:49:23 -0800

    remove max min

--- 4096_t/4096_t.h
+++ 4096_t/4096_t.h
@@ -8,7 +8,6 @@
 #define S (size_t)(4096 / 64)
 #define BYTES S * sizeof(uint64_t)
 
-void seebig(uint64_t *a); 
 uint64_t bigadd(uint64_t *in0, uint64_t *in1, uint64_t *sum); 
 uint64_t bigsub(uint64_t *min, uint64_t *sub, uint64_t *dif); 
 uint64_t bigmul(uint64_t *in0, uint64_t *in1, uint64_t *out); 
