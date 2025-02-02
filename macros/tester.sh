20ccbc1fd9b0b679e1e51f04adbd12a91e8d32fe
calvin <cd-public@users.noreply.github.com>
2025-02-01 16:54:59 -0800

    Update tester.sh

--- macros/tester.sh
+++ macros/tester.sh
@@ -1,1 +1,1 @@
-cat <(grep -i "CHOICE" macros.c) <(grep -i "MEDIAN" macros.c) <(grep -i "ROTATE" macros.c) tester.c | gcc -xc - -Wno-pointer-to-int-cast -o test ; ./test ; echo $?
+cat <(grep -i "#define CHOICE" macros.c) <(grep -i "#define MEDIAN" macros.c) <(grep -i "#define ROTATE" macros.c) tester.c | gcc -xc - -Wno-pointer-to-int-cast -o test ; ./test ; echo $?
