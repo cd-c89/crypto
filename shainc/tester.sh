9f94bd6b4cc6f942fce1d1fb9318144ee60d0b95
calvin <cd-public@users.noreply.github.com>
2025-02-09 11:29:25 -0800

    Update tester.sh
    Removing C89 req for CS 540

--- shainc/tester.sh
+++ shainc/tester.sh
@@ -1,4 +1,4 @@
-gcc shainc.c --std=c89 -Wall -Wextra -Werror -Wpedantic -O2 -o shainc
+gcc shainc.c -Wall -Wextra -Werror -Wpedantic -O2 -o shainc
 echo "15 characters." > 15char.txt
 echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." > lipsum.txt
 curl https://github.com/cd-public/books/raw/main/pg1342.txt -o austen.txt 2>/dev/null
