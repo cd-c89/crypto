f6ff589f4be342628141b2ed11b0d160167b3d99
procal <cd@laptop.wsl>
2025-03-31 09:38:01 -0700

    list_t

---
+++ list_t/list_t.c
@@ -0,0 +1,76 @@
+// list_t.c
+
+// imlistements functions in list_t.h
+
+#include "list_t.h"
+
+struct list_t_struct
+{
+	void *data;
+	list_t next;
+};
+
+list_t list_new()
+{
+	return NULL;
+}
+
+void list_free(list_t l)
+{
+	return;
+}
+
+void list_print(list_t l)
+{
+	return;
+}
+
+void list_append(list_t l, void *x)
+{
+	return;
+}
+
+void list_extend(list_t l1, list_t l2)
+{
+	return;
+}
+
+void list_insert(list_t l, uint64_t i, void *x)
+{
+	return;
+}
+
+bool list_remove(list_t l, void *x)
+{
+	return FALSE;
+}
+
+void *list_pop(list_t l, uint64_t i)
+{
+	return NULL;
+}
+
+void list_clear(list_t l)
+{
+	return;
+}
+
+uint64_t list_index(list_t l, void *x)
+{
+	return -1;
+}
+
+uint64_t list_count(list_t l, void *x)
+{
+	return 0;
+}
+
+void list_reverse(list_t l)
+{
+	return;
+}
+
+list_t list_copy(list_t l)
+{
+	return NULL;
+}
