bf2702deedef25449b0b32f8f1ede1db5574f58b
procal <cd@laptop.wsl>
2025-03-31 10:30:21 -0700

    list_t

--- list_t/list_t.c
+++ list_t/list_t.c
@@ -1,76 +1,76 @@
-// list_t.c
+/* list_t.c */
 
-// implements functions in list_t.h
+/* implements functions in list_t.h */
 
 #include "list_t.h"
 
 struct list_t_struct
 {
-	void *data;
-	list_t next;
+    void *data;
+    list_t next;
 };
 
 list_t list_new()
 {
-	return NULL;
+    return NULL;
 }
 
 void list_free(list_t l)
 {
-	return;
+    return;
 }
 
 void list_print(list_t l)
 {
-	return;
+    return;
 }
 
 void list_append(list_t l, void *x)
 {
-	return;
+    return;
 }
 
 void list_extend(list_t l1, list_t l2)
 {
-	return;
+    return;
 }
 
 void list_insert(list_t l, uint64_t i, void *x)
 {
-	return;
+    return;
 }
 
 bool list_remove(list_t l, void *x)
 {
-	return FALSE;
+    return FALSE;
 }
 
 void *list_pop(list_t l, uint64_t i)
 {
-	return NULL;
+    return NULL;
 }
 
 void list_clear(list_t l)
 {
-	return;
+    return;
 }
 
 uint64_t list_index(list_t l, void *x)
 {
-	return -1;
+    return -1;
 }
 
 uint64_t list_count(list_t l, void *x)
 {
-	return 0;
+    return 0;
 }
 
 void list_reverse(list_t l)
 {
-	return;
+    return;
 }
 
 list_t list_copy(list_t l)
 {
-	return NULL;
+    return NULL;
 }
